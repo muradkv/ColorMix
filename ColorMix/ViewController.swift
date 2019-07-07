be19a5b70173d0444981ad886354c4199d3350f8
murad <muradkv@gmail.com>
2019-07-07 23:27:07 +0300

    Add view and 3 switch that can change color of the view

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSwitch: UISwitch!
+    @IBOutlet var greenSwitch: UISwitch!
+    @IBOutlet var blueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        updateColor()
     }
 
-
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn {
+            red = 1
+        }
+        
+        if greenSwitch.isOn {
+            green = 1
+        }
+        
+        if blueSwitch.isOn {
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+        colorView.backgroundColor = color
+        colorView.layer.borderWidth = 1
+        colorView.layer.cornerRadius = 10
+        colorView.layer.borderColor = UIColor.gray.cgColor
+    }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        
+        updateColor()
+        
+    }
+    
 }
 
